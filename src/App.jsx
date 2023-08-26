6dc5063b1f3937b16789f8bcc5f3611757602448
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2023-08-26 19:08:27 +0400

    Added Data.Json file and addLinks component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './components/authentication/Login'
-import Signup from './components/authentication/Signup'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/authentication/Login";
+import Signup from "./components/authentication/Signup";
+import "./App.css";
+import { AddLinks } from "./components/links/addLinks";
 
 function App() {
-
   return (
-      <Routes>
-        <Route path='/' element={<Navigate to="/login" replace />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/addLinks" element={<AddLinks />} />
+    </Routes>
   );
 }
 
