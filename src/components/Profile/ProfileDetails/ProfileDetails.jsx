57099616339ce5d526eaf23cc7fe472572564c22
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-27 00:23:13 +0400

    add phone image for desktop resolution

--- src/components/Profile/ProfileDetails/ProfileDetails.jsx
+++ src/components/Profile/ProfileDetails/ProfileDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "./illustration-phone-mockup.svg";
 import Styles from "./ProfileDetails.module.css";
 import ProfileImage from "./ProfileImage/ProfileImage";
 import ProfileForm from "./ProfileForm/ProfileForm";
@@ -6,6 +7,9 @@ import ProfileForm from "./ProfileForm/ProfileForm";
 const ProfileDetails = ({ title, content }) => {
   return (
     <div className={Styles.body}>
+      <div className={Styles.desktop}>
+        <img src={Image} alt="Image" />
+      </div>
       <div className={Styles.main}>
         <div className={Styles.text_content}>
           <h1 className={Styles.title}>{title}</h1>
