864d3538cda216664b62290d5af63df7fc6491f1
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-08-27 17:16:41 +0400

    install and use react-hook-form

--- src/components/Profile/ProfileForm/ProfileForm.jsx
+++ src/components/Profile/ProfileForm/ProfileForm.jsx
@@ -1,24 +1,43 @@
-import React from "react";
 import Styles from "./ProfileForm.module.css";
+import { useForm } from "react-hook-form";
 
 const ProfileForm = ({ firstName, setFirstName }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <div className={Styles.container}>
         <div className={Styles.main}>
-          <form className={Styles.form}>
+          <form onSubmit={handleSubmit(onSubmit)} className={Styles.form}>
             <div className={Styles.firstName_input_box}>
               {/* FirstName */}
               <label htmlFor="firstName" className={Styles.label}>
                 First Name*
               </label>
-              <input
-                type="text"
-                placeholder="e.g. John"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                className={Styles.input}
-              />
+              <div className={Styles.firstName_input_content}>
+                <input
+                  type="text"
+                  placeholder="e.g. John"
+                  value={firstName}
+                  {...register("firstName", {
+                    required: {
+                      value: true,
+                      message: "Username is required",
+                    },
+                  })}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className={Styles.input}
+                />
+                <p className={Styles.error_message}>
+                  {errors.firstName?.message}
+                </p>
+              </div>
             </div>
             <div className={Styles.firstName_input_box}>
               {/* LastName */}
@@ -29,6 +48,7 @@ const ProfileForm = ({ firstName, setFirstName }) => {
                 type="text"
                 placeholder="e.g. Appleseed"
                 className={Styles.input}
+                {...register("lastName")}
               />
             </div>
             <div className={Styles.firstName_input_box}>
@@ -40,8 +60,10 @@ const ProfileForm = ({ firstName, setFirstName }) => {
                 type="text"
                 placeholder="e.g. email@example.com"
                 className={Styles.input}
+                {...register("email")}
               />
             </div>
+            <button type="submit">Submit</button>
           </form>
         </div>
       </div>
