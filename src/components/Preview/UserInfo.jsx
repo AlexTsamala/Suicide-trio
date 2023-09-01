14cd4ea3983867028023fc80ee3a919980b7207b
Dato Qevxishvili <qevxishvili.dato99@gmail.com>
2023-09-01 23:52:50 +0400

    update

--- src/components/Preview/UserInfo.jsx
+++ src/components/Preview/UserInfo.jsx
@@ -1,25 +1,40 @@
+import { useEffect, useState } from "react";
 import Profile from "../../assets/images/user-profile-image.jpg";
 import Styles from "./UserInfo.module.css";
 import Github from "../../assets/images/icon-github.svg";
 import Arrow from "../../assets/images/icon-arrow-right.svg";
 import Data from "../../../Data.json";
 const UserInfo = () => {
+  const [currentProfile, setCurrentProfile] = useState();
   const currentUserId = JSON.parse(localStorage.getItem("currentUser")).UserId;
-  const currentProfile = Data.Profile.find(
-    (item) => item.userId === currentUserId
-  );
+
+  useEffect(() => {
+    const currentProfilee = Data.Profile.find(
+      (item) => item.userId === currentUserId
+    );
+    if (currentProfilee) {
+      setCurrentProfile(currentProfilee);
+    } else {
+      setCurrentProfile({
+        firstName: "",
+        lastName: "",
+        email: "",
+        imgUrl: "",
+      });
+    }
+  }, []);
   return (
     <div className={Styles.main_container}>
       <div className={Styles.user_info}>
         <img
-          src={currentProfile.imgUrl}
+          src={currentProfile?.imgUrl}
           alt="Profile"
           className={Styles.user_profile_img}
         />
         <h1 className={Styles.user_name}>
-          {currentProfile.firstName + " " + currentProfile.lastName}
+          {currentProfile?.firstName + " " + currentProfile?.lastName}
         </h1>
-        <h2 className={Styles.user_email}>{currentProfile.email}</h2>
+        <h2 className={Styles.user_email}>{currentProfile?.email}</h2>
       </div>
 
       {/* Socials */}
